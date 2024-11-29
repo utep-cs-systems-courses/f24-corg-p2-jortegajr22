0fb13f769e3e2cdc7d26d9be7f0e9c0279d85dc5
jortegajr22 <jortega22@miners.utep.edu>
2024-11-29 16:20:39 -0700

    make sw1&sw2 work

--- project/projMain/main.c
+++ project/projMain/main.c
@@ -3,18 +3,46 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
-int
-main()
+#define LED BIT6
+
+#define SW1 1
+#define SW2 2
+#define SW3 4
+#define SW4 8
+
+#define SWITCHES 15
+
+
+int col = 0, row = 0, int currCol = 0;
+int centerCol = width/2, centerRow = height-12;
+
+static char
+switch_update_interupt_sense()
 {
-  configureClocks();
-  lcd_init();
-  u_char width = screenWidth, height = screenHeight;
+  char p2val = P2IN;
+  /* update switch iinterrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);   /* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);  /* if switch down, sense up */
+  return p2val;
+}
 
-  clearScreen(COLOR_BLUE);
+void
+switch_init()
+{
+  P2REN |= SWITCHES;             /* enables resistors for switches */
+  P2IE |= SWITCHES;              /* enable interrupts from switches */
+  P2OUT |= SWITCHES;             /* pull-ups for switches */
+  P2DIR &= ~SWITCHES            ;/* set switches' bits for input */
+  switch_update_interrupt_sense();
+}
+int switches = 0;
 
-  int col = 0, row = 0;
-  int centerCol = width/2, centerRow = height-12;
-  for (row = 0; row < 5; row ++){
+
+
+void
+draw_shape()
+{
+  for (row; row < 5; row ++){
     for (col = -row; col <= row/4; col ++)
       drawPixel(centerCol + col, centerRow + row, COLOR_YELLOW);
     for (; col <= row; col++)
@@ -25,5 +53,25 @@ main()
       drawPixel(centerCol + col, centerRow + row, COLOR_YELLOW);
     for (; col <= row; col++)
       drawPixel(centerCol + col, centerRow +row, COLOR_RED);
+  }
+}
+
+int
+main()
+{
+  configureClocks();
+  lcd_init();
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLUE);
+
+  draw_shape();
+}
+
+void
+__interupt_vec(PORT2_VECTOR) Port_2(){
+    if (P2IFG & SWITCHES) {      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;          /* clear pending sw interrupts */
+    switch_interrupt_handler();  /* single handler for all switches */
   }
 }
